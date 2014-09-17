04ff2fb8780dc7208e40dbbddd8b42ac23f12c6d
sewerside <sewerside@gmail.com>
2014-09-17 16:21:41 +0800

    Test

---
+++ api.php
@@ -0,0 +1,25 @@
+<?php
+// Backend API for Life Shield
+
+/* TODO
+
+Create tables:
+- Alert
+- Resident
+- Guard
+
+Mechanics:
+- Visitor come, tell guard the visit address to visit and provide car number plate and name (optional)
+- Guard register the car no. plate, name and select the visit address. A Push Notification will be sent to the resident
+-
+
+
+*/
+
+ini_set("display_errors", 1);
+error_reporting(E_ALL ^ E_NOTICE);
+date_default_timezone_set("Asia/Kuala_Lumpur");
+
+
+
+?>
